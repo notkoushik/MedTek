9b957fcf1883e8b91ae15805ce6f444e57b9693c
notkoushik <nanikoushik96@gmail.com>
2026-01-02 18:23:16 +0530

    Description of your changes

--- medtek/macos/Flutter/GeneratedPluginRegistrant.swift
+++ medtek/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -7,6 +7,7 @@ import Foundation
 
 import file_selector_macos
 import flutter_image_compress_macos
+import flutter_tts
 import geolocator_apple
 import shared_preferences_foundation
 import speech_to_text
@@ -14,6 +15,7 @@ import speech_to_text
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterImageCompressMacosPlugin.register(with: registry.registrar(forPlugin: "FlutterImageCompressMacosPlugin"))
+  FlutterTtsPlugin.register(with: registry.registrar(forPlugin: "FlutterTtsPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
